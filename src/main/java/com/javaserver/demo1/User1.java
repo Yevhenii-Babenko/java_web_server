1e72dc16f7de6e470fef1e2e947b419fc13c5492
ybabenkoJapp <ybabenko@jappware.com>
2023-03-16 21:24:46 +0200

    refactor(User1.java): rename file name

--- src/main/java/com/javaserver/demo1/User1.java
+++ src/main/java/com/javaserver/demo1/User1.java
@@ -1,12 +1,12 @@
 package com.javaserver.demo1;
 
-public class User {
+public class User1 {
     private int id;
     private String firstName;
     private String lastName;
     private String email;
 
-    public User(int id, String firstName, String lastName, String email) {
+    public User1(int id, String firstName, String lastName, String email) {
         this.id = id;
         this.firstName = firstName;
         this.lastName = lastName;
