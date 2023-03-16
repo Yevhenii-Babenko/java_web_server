74d51c72c2745a7849402a367ea64e14a4fd52f3
ybabenkoJapp <ybabenko@jappware.com>
2023-03-16 21:31:00 +0200

    refactor(UserRepository.java): remove JpaRepository, add CrudRepository

--- src/main/java/com/javaserver/demo1/repository/UserRepository.java
+++ src/main/java/com/javaserver/demo1/repository/UserRepository.java
@@ -1,9 +1,10 @@
 package com.javaserver.demo1.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import com.javaserver.demo1.model.User;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long>{
+public interface UserRepository extends CrudRepository<User, Long>{
 }
 
