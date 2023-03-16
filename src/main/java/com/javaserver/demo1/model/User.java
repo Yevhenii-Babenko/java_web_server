6d7437c75e2a564d59acfa661680e462ce482ee7
ybabenkoJapp <ybabenko@jappware.com>
2023-03-16 21:28:48 +0200

    feat(User.java): override imports

--- src/main/java/com/javaserver/demo1/model/User.java
+++ src/main/java/com/javaserver/demo1/model/User.java
@@ -1,5 +1,10 @@
 package com.javaserver.demo1.model;
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.Column;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 
 @Entity
 @Table(name = "user")
