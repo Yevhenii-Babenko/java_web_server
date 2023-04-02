38313078b3190e6aae2138ff9b96f45e7a68291b
ybabenkoJapp <ybabenko@jappware.com>
2023-04-02 15:57:23 +0300

    refactor(Demo1Application.java): remove commented code

--- src/main/java/com/javaserver/demo1/Demo1Application.java
+++ src/main/java/com/javaserver/demo1/Demo1Application.java
@@ -8,10 +8,6 @@ import  com.javaserver.demo1.database.DatabaseConnectionManager;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
-
-import java.sql.Connection;
-
-//@EnableJpaRepositories(basePackages="com.javaserver.demo1.repository.UserRepository")
 @EnableAutoConfiguration
 @SpringBootApplication
 @ComponentScan(basePackages = "com.javaserver.demo1")
@@ -24,16 +20,5 @@ public class Demo1Application {
 //        Connection conn = db.connect_to_db("postgres", "postgres", "root");
         DatabaseConnectionManager database = new DatabaseConnectionManager();
         database.checkConnection();
-//		db.createTable(conn,"employee");
-//		db.insert_row(conn, "employee", "rajat", "india");
-//		db.update_name(conn,"employee","rahul", "rahul1");
-//		db.search_by_name(conn, "employee", "rajat");
-//		db.search_by_id(conn, "employee", 4);
-//		db.read_data(conn,"employee");
-//		db.delete_row_by_name(conn, "employee", "rajat");
-//		db.delete_row_by_id(conn, "employee", 2);
-//		db.read_data(conn,"employee");
-//		db.delete_table(conn, "employee");
     }
-// user serv
 }
