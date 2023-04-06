4500fedeb5b1fad93d7662b95830717f95b8ef4b
ybabenkoJapp <ybabenko@jappware.com>
2023-04-06 11:37:28 +0300

    ашч(Demo1Application.java, pom.xml): fixed versions mismatch and fail of repository

--- src/main/java/com/javaserver/demo1/Demo1Application.java
+++ src/main/java/com/javaserver/demo1/Demo1Application.java
@@ -3,16 +3,18 @@ package com.javaserver.demo1;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @Configuration
 @ComponentScan
 @EnableAutoConfiguration(exclude = { DataSourceAutoConfiguration.class })
+@EntityScan("entity")
+@EnableJpaRepositories("com.javaserver.demo1.repository")
 @SpringBootApplication
-//@EntityScan("com.javaserver.*")
-//@ComponentScan(basePackages = "com.javaserver.demo1")
 public class Demo1Application {
     public static void main(String[] args) {
         SpringApplication.run(Demo1Application.class, args);
