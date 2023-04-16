bfd1055a4305df1f7d732eee02855bbb73443b4e
ybabenkoJapp <ybabenko@jappware.com>
2023-04-16 07:03:24 +0300

    refactor(UserRepository.java): add JpaRepository interface

--- src/main/java/com/javaserver/demo1/repository/UserRepository.java
+++ src/main/java/com/javaserver/demo1/repository/UserRepository.java
@@ -1,10 +1,12 @@
 package com.javaserver.demo1.repository;
 
 import com.javaserver.demo1.model.User;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.stereotype.Repository;
 
+@EnableJpaRepositories("es.uc3m.tiw.dominios")
 @Repository
-public interface UserRepository extends CrudRepository<User, Long>{
+public interface UserRepository extends JpaRepository<User, Long> {
 }
 
