1a5895fe15bc16e34a06b09262334a1ff8590a9e
ybabenkoJapp <ybabenko@jappware.com>
2023-04-26 00:35:47 +0300

    fix(Product.java): resolve no default constructor for entity for product model

--- src/main/java/com/javaserver/demo1/model/Product.java
+++ src/main/java/com/javaserver/demo1/model/Product.java
@@ -1,10 +1,16 @@
 package com.javaserver.demo1.model;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 import javax.persistence.Id;
 
 @Entity
 @Table(name = "products", schema = "public")
+@Data
+@NoArgsConstructor
 public class Product {
     @Id
     private Long id;
