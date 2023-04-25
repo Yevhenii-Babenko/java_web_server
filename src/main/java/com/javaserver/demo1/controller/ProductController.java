96c1bf9f41056e73f401268c188a730b115d1a58
ybabenkoJapp <ybabenko@jappware.com>
2023-04-26 00:36:41 +0300

    feat(ProductController.java): update rest annotations and add post method

--- src/main/java/com/javaserver/demo1/controller/ProductController.java
+++ src/main/java/com/javaserver/demo1/controller/ProductController.java
@@ -3,20 +3,24 @@ package com.javaserver.demo1.controller;
 import com.javaserver.demo1.model.Product;
 import com.javaserver.demo1.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/products")
+@RequestMapping("/api/v1")
 public class ProductController {
     @Autowired
     private ProductService productService;
 
-    @GetMapping
+    @GetMapping("products")
     public List<Product> getAllProducts() {
         return productService.getAllProducts();
+    }
+
+    // save products
+    @PostMapping("products")
+    public Product createProduct(@RequestBody Product postProduct) {
+        return productService.createProduct(postProduct);
     }
 }
