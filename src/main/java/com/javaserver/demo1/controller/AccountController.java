6dadc3ea08417ac340b46db8286839849a631e7a
ybabenkoJapp <ybabenko@jappware.com>
2023-06-14 22:55:01 +0300

    feat(AccountController.java, AccountRepository.java): add account controller and repository

---
+++ src/main/java/com/javaserver/demo1/controller/AccountController.java
@@ -0,0 +1,71 @@
+package com.javaserver.demo1.controller;
+
+import com.javaserver.demo1.model.Account;
+import com.javaserver.demo1.repository.AccountRepository;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import com.javaserver.demo1.JwtResponse;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import javax.servlet.annotation.WebFilter;
+
+import java.util.Date;
+
+@RestController
+@RequestMapping("/api/v1")
+@RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:5173/") // Add this line to allow requests from your Vue application
+public class AccountController {
+
+    private final AccountRepository accountRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
+    private final UserDetailsService userDetailsService;
+    @PostMapping("/register")
+    public Account postRegisterAccount(@RequestBody Account registerAccount) {
+        return this.accountRepository.save(registerAccount);
+    }
+
+    private String generateJwtToken(String username) {
+        String secretKey = "webProjectKey"; // Replace with your own secret key
+
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 864000000)) // Set token expiration time (e.g., 10 days)
+                .signWith(SignatureAlgorithm.HS512, secretKey)
+                .compact();
+    }
+
+@PostMapping("/login")
+    public ResponseEntity<?> postLoginAccount(@RequestBody Account loginAccount) {
+        try {
+            // Authenticate the user
+            Authentication authentication = authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(
+                            loginAccount.getUsername(),
+                            loginAccount.getPassword()
+                    )
+            );
+
+            // Generate JWT token for the authenticated user
+            UserDetails userDetails = userDetailsService.loadUserByUsername(loginAccount.getUsername());
+            String token = generateJwtToken(userDetails.getUsername());
+
+            // Return the JWT token as the response
+            return ResponseEntity.ok(new JwtResponse(token));
+        } catch (AuthenticationException e) {
+            // Handle authentication failure
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+    }
+}
