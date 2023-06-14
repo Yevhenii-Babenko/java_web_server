7eb57b461071e89a20ddd0df07c504308b2d5782
ybabenkoJapp <ybabenko@jappware.com>
2023-06-14 23:01:50 +0300

    feat( UserDetailsServiceImpl.java ) add user details service

---
+++ src/main/java/com/javaserver/demo1/service/UserDetailsServiceImpl.java
@@ -0,0 +1,29 @@
+package com.javaserver.demo1.service;
+
+import com.javaserver.demo1.model.Account;
+import com.javaserver.demo1.repository.AccountRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final AccountRepository accountRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Account account = accountRepository.findByUsername(username);
+        if (account == null) {
+            throw new UsernameNotFoundException("User not found with username: " + username);
+        }
+        return org.springframework.security.core.userdetails.User
+                .withUsername(username)
+                .password(account.getPassword())
+                .roles("USER")
+                .build();
+    }
+}
