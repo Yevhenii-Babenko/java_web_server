fe910137e283932e853eeedb55b45f053349812e
ybabenkoJapp <ybabenko@jappware.com>
2023-06-16 22:37:53 +0300

    refactor(AccountController.java): add BCryptPasswordEncoder, update coding JWT algorithm

--- src/main/java/com/javaserver/demo1/controller/AccountController.java
+++ src/main/java/com/javaserver/demo1/controller/AccountController.java
@@ -5,6 +5,7 @@ import com.javaserver.demo1.repository.AccountRepository;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,9 +16,10 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import javax.servlet.annotation.WebFilter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import io.jsonwebtoken.security.Keys;
 
+import java.util.Base64;
 import java.util.Date;
 
 @RestController
@@ -27,42 +29,55 @@ import java.util.Date;
 public class AccountController {
 
     private final AccountRepository accountRepository;
-    private final PasswordEncoder passwordEncoder;
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
     private final AuthenticationManager authenticationManager;
     private final UserDetailsService userDetailsService;
+
     @PostMapping("/register")
     public Account postRegisterAccount(@RequestBody Account registerAccount) {
-        return this.accountRepository.save(registerAccount);
+        String encodedPassword = passwordEncoder.encode(registerAccount.getPassword());
+        registerAccount.setPassword(encodedPassword);
+        return accountRepository.save(registerAccount);
     }
 
     private String generateJwtToken(String username) {
-        String secretKey = "webProjectKey"; // Replace with your own secret key
+        byte[] keyBytes = Keys.secretKeyFor(SignatureAlgorithm.HS512).getEncoded();
+        String base64Key = Base64.getEncoder().encodeToString(keyBytes);
+//        String secretKey = "webProjectKey"; // Replace with your own secret key
 
-        return Jwts.builder()
-                .setSubject(username)
-                .setIssuedAt(new Date())
-                .setExpiration(new Date(System.currentTimeMillis() + 864000000)) // Set token expiration time (e.g., 10 days)
-                .signWith(SignatureAlgorithm.HS512, secretKey)
-                .compact();
+        return Jwts.builder().setSubject(username).setIssuedAt(new Date()).setExpiration(new Date(System.currentTimeMillis() + 864000000)) // Set token expiration time (e.g., 10 days)
+                .signWith(SignatureAlgorithm.HS512, base64Key).compact();
     }
 
-@PostMapping("/login")
+    @PostMapping("/login")
     public ResponseEntity<?> postLoginAccount(@RequestBody Account loginAccount) {
         try {
-            // Authenticate the user
-            Authentication authentication = authenticationManager.authenticate(
-                    new UsernamePasswordAuthenticationToken(
-                            loginAccount.getUsername(),
-                            loginAccount.getPassword()
-                    )
-            );
+            // Retrieve the user account from the database
+            Account userAccount = accountRepository.findByUsername(loginAccount.getUsername());
 
-            // Generate JWT token for the authenticated user
-            UserDetails userDetails = userDetailsService.loadUserByUsername(loginAccount.getUsername());
-            String token = generateJwtToken(userDetails.getUsername());
+            if (userAccount == null) {
+                // User account not found, return UNAUTHORIZED response
+                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+            }
 
-            // Return the JWT token as the response
-            return ResponseEntity.ok(new JwtResponse(token));
+            // Compare the encoded password stored in the database with the user's input
+            if (passwordEncoder.matches(loginAccount.getPassword(), userAccount.getPassword())) {
+                // Passwords match, proceed with authentication
+
+                // Authenticate the user
+                Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(loginAccount.getUsername(), loginAccount.getPassword()));
+
+                // Generate JWT token for the authenticated user
+                UserDetails userDetails = userDetailsService.loadUserByUsername(loginAccount.getUsername());
+                String token = generateJwtToken(userDetails.getUsername());
+
+                // Return the JWT token as the response
+                return ResponseEntity.ok(new JwtResponse(token));
+            } else {
+                // Passwords do not match, return UNAUTHORIZED response
+                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+            }
         } catch (AuthenticationException e) {
             // Handle authentication failure
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
